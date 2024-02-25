2583a69bffaf7e5d18bbecf6b52d5619377bca6e
19azmi <149889364+19azmi@users.noreply.github.com>
2024-02-25 12:45:30 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+a= 10 
+print("==aplikasi biodata===")
+
+nama= input("masukan nama :")
+gender= input (: masukan nama:  ")
+               
+tanggal lahir =inputb ("masukan tanggal lahir")       
+hobi ("hobi")
+
+print ("nama")
+print ("gender")
+prin ("tanggal lahir")
+prin ("hobi")
+
+               
